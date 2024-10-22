7f7cf73c20b192371c6ebfcc8850168530e008a4
lanskaianadezhda <nadin-91@bk.ru>
2024-10-22 17:17:19 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //task 1
+        System.out.println("Задача 1");
+    }
+
+    public static void printLeapYear(int year) {
+
+    }
+}
